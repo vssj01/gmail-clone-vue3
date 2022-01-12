52bcb3b2eefa0a71619eb49d34f37be1a002dba2
vssj01 <vssj01@gmail.com>
2022-01-12 17:13:17 +0530

    Add Bulk actions and archive view

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <h1>VMail Inbox</h1>
+
+  <h1>App: {{emailSelection.emails.size}} emails selected</h1>
+
   <Suspense>
     <template #default>
       <MailTable />
     </template>
-    <template #fallback>Loading...</template>
+    <template #fallback>
+      Loading...
+    </template>
   </Suspense>
-
-
 </template>
   
 <script>
-import MailTable from './components/MailTable.vue'
+import MailTable from '@/components/MailTable.vue';
+import useEmailSelection from '@/composables/use-email-selection';
 export default {
   name: 'App',
   components: {
     MailTable
+  },
+  setup(){
+    return {
+      emailSelection: useEmailSelection()
+    }
   }
-
 };
 </script>
 
@@ -30,9 +38,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
 /* Global Styles */
-
 button {
   font-size: 16px;
   padding: 8px;
@@ -40,22 +46,18 @@ button {
   margin: 5px 10px 5px 0px;
   cursor: pointer;
 }
-
 button:disabled {
   cursor: auto;
 }
-
 button.selected {
   cursor: auto;
   color: black;
   border-color: black;
   border-width: 2px;
 }
-
 .clickable {
   cursor: pointer;
 }
-
 input[type='checkbox'] {
   -webkit-appearance:none;
   cursor: pointer;
@@ -68,21 +70,16 @@ input[type='checkbox'] {
   vertical-align: middle;
   padding: 10px;
 }
-
 input[type='checkbox'].partial-check {
   background: #ABC;
 }
-
 input[type='checkbox']:checked {
   background: #679;
 }
-
 .mb-0 {
   margin-bottom: 0;
 }
-
 /* Modal */
-
 .modal, .overlay {
   width: 100%;
   height: 100%;
@@ -105,15 +102,11 @@ input[type='checkbox']:checked {
   z-index: 10;
   opacity: 1;
 }
-
 /* Email Modal */
-
 .email-display {
   text-align: left;
 }
-
 /* Mail Table */
-
 .mail-table {
   max-width: 1000px;
   margin: auto;
@@ -138,13 +131,10 @@ input[type='checkbox']:checked {
   overflow-y: hidden;
   margin: 0;
 }
-
 .mail-table td.date {
   width: 120px;
 }
-
 /* Bulk Action Bar */
-
 .bulk-action-bar {
   width: 100%;
   max-width: 1000px;
@@ -152,11 +142,9 @@ input[type='checkbox']:checked {
   text-align: left;
   padding-bottom: 8px;
 }
-
 .bulk-action-bar input {
   margin: 5px;
 }
-
 .bulk-action-bar .checkbox {
   margin-right: 6px;
   margin-left: 3px;
